a77a0ed15f9239da57f96a920ba96916aef802be
MachiRC <macks1006@gmail.com>
2025-11-20 21:46:04 -0500

    Add Super Smash Bros. 64 to ROM list

--- docs/dist/romlist.js
+++ docs/dist/romlist.js
@@ -2,4 +2,5 @@ var ROMLIST = [
     {url:"roms/Kart64.z64",title:"Mario Kart 64"},
     {url:"roms/LOZMajora.z64",title:"Legend of Zelda: Majora's Mask"},
     {url:"roms/LOZOcarina.z64",title:"Legend of Zelda: Ocarina of Time"},
+    {url:"roms/Smash_Remix_2.0.0.z64",title:"Super Smash Bros. 64"},
 ];
